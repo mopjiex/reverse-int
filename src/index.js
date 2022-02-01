3287f6e49ed750ac626a1934095a335d7d751b41
mopjiex <artem1bannikov@gmail.com>
2022-02-01 10:43:44 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-    return +(String(n).split('').reverse().join(''));
+    return +Math.abs(n)
+            .toString().split('')
+            .reverse().join('');
 }
+
+
